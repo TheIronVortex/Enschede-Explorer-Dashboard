e56cebe590fdafbd3629e9811d990cd995c7a40e
TheIronVortex <sem.opstals@student.gildeopleidingen.nl>
2024-03-22 13:41:27 +0100

    User & POI v1
    First implementation of Users and POI's

--- src/Dashboard/POIData.js
+++ src/Dashboard/POIData.js
@@ -1,32 +1,271 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import GetData from "./GatherData";
-import { ListGroup } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
+import Card from "react-bootstrap/esm/Card";
 import BreadcrumbNav from "../Navbar/BreadcrumNav";
+import Form from "react-bootstrap/esm/Form";
+import { ref, set } from "firebase/database";
+import db from "../FirebaseInit";
 
-function POIData() {
-  const { parentKeys, data } = GetData();
 
+function UserData() {
+  const Description = 0
+  const Location = 1
+  const PoiID = 2
+  const PoiName = 3
+  const PoiType = 4
+
+  const { parentKeys, data, ValueName } = GetData();
+  const [editableFields, setEditableFields] = useState({
+    [parentKeys[2]]: false,
+    [parentKeys[1]]: false,
+  });
+  const progressKeys = data ? Object.keys(data[parentKeys[3]]) : [];
+  const progressValues = data ? Object.values(data[parentKeys[3]]) : [];
+  const [editedValues, setEditedValues] = useState({
+    [parentKeys[2]]: "",
+    [parentKeys[1]]: "",
+  });
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (data !== null) {
+      setLoading(false);
+    }
+  }, [data]);
+
+  const toggleEditMode = (field) => {
+    setEditableFields({
+      ...editableFields,
+      [field]: !editableFields[field],
+    });
+  };
+
+  const saveData = (field, value) => {
+    if (value !== undefined) {
+      // Update player name
+      if (field === parentKeys[2]) {
+        //setParentKey(value); // Update parentKey state
+      }
+      var inputData = value;
+      const dataToUpdate = {};
+      dataToUpdate[field] = inputData;
+      if (progressKeys.includes(field)) {
+        inputData = parseInt(value);
+        set(ref(db, `Users/${ValueName}/Progression/${field}`), inputData);
+      } else {
+        set(ref(db, `Users/${ValueName}/${field}`), inputData);
+      }
+    }
+  };
+
+  const handleInputChange = (field, value) => {
+    setEditedValues({
+      ...editedValues,
+      [field]: value,
+    });
+  };
+
+  const handleSaveProgress = () => {
+    // Save all progress keys and values
+    Object.keys(editedValues[parentKeys[3]]).forEach((key) => {
+      saveData(key, editedValues[parentKeys[3]][key]);
+    });
+    // Toggle edit mode for all progress keys
+    toggleEditMode(parentKeys[3]);
+  };
+
+  if (loading) {
+    return <div>Fetching Data...</div>;
+  }
+  
   return (
-    <div>
+    <>
       <Row>
         <Col>
           <BreadcrumbNav />
         </Col>
       </Row>
-      <h1>{data && data.PoiName}</h1>
-      {data && (
-        <ListGroup>
-          {parentKeys.map((key, index) => (
-            <ListGroup.Item key={index}>
-              {key}: {typeof data[key] === "object" ? JSON.stringify(data[key]) : data[key]}
-            </ListGroup.Item>
-          ))}
-        </ListGroup>
-      )}
-    </div>
-  );  
+      {/*
+        <h1>{data && data.PoiName}</h1>
+        {data && (
+          <ListGroup>
+            {parentKeys.map((key, index) => (
+              <ListGroup.Item key={index}>
+                {key}: {typeof data[key] === "object" ? JSON.stringify(data[key]) : data[key]}
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      */}
+      <Row>
+        <Col>
+          <Card className="p-5 bg-secondary rounded-5">
+            <Row>
+              <Col>
+                <h1 className="pb-3 text-light">{data && data[parentKeys[PoiName]]}</h1>
+              </Col>
+            </Row>
+            <Row>
+              <Col md className="bg-white rounded-start-5 p-3">
+                <Form>
+                  <Form.Group>
+                    <Form.Label>{parentKeys[PoiID]}:</Form.Label>
+                    <div className="d-flex align-items-center">
+                      <Form.Control
+                        disabled
+                        type="text"
+                        className="ms-4 bg-light"
+                        style={{ width: "95%" }}
+                        defaultValue={data && data[parentKeys[PoiID]]}
+                      />
+                    </div>
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label>{parentKeys[PoiName]}:</Form.Label>
+                    <div className="d-flex align-items-center">
+                      <Form.Control
+                        disabled={!editableFields[parentKeys[PoiName]]}
+                        type="text"
+                        className="ms-4 bg-light"
+                        style={{ width: "95%" }}
+                        defaultValue={data && data[parentKeys[PoiName]]}
+                        onChange={(e) =>
+                          handleInputChange(
+                            parentKeys[PoiName],
+                            e.target.value
+                          )
+                        }
+                      />
+                      <Button
+                        variant="secondary"
+                        className="ms-2"
+                        onClick={() => {
+                          if (editableFields[parentKeys[PoiName]]) {
+                            saveData(
+                              parentKeys[PoiName],
+                              editedValues[parentKeys[PoiName]]
+                            );
+                          }
+                          toggleEditMode(parentKeys[PoiName]);
+                        }}
+                      >
+                        {editableFields[parentKeys[PoiName]] ? "Save" : "Edit"}
+                      </Button>
+                    </div>
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label>{parentKeys[PoiType]}:</Form.Label>
+                    <div className="d-flex align-items-center">
+                      <Form.Control
+                        disabled={!editableFields[parentKeys[PoiType]]}
+                        type="text"
+                        className="ms-4 bg-light"
+                        style={{ width: "95%" }}
+                        defaultValue={data && data[parentKeys[PoiType]]}
+                        onChange={(e) =>
+                          handleInputChange(
+                            parentKeys[PoiType],
+                            e.target.value
+                          )
+                        }
+                      />
+                      <Button
+                        variant="secondary"
+                        className="ms-2"
+                        onClick={() => {
+                          if (editableFields[parentKeys[PoiType]]) {
+                            saveData(
+                              parentKeys[PoiType],
+                              editedValues[parentKeys[PoiType]]
+                            );
+                          }
+                          toggleEditMode(parentKeys[PoiType]);
+                        }}
+                      >
+                        {editableFields[parentKeys[PoiType]] ? "Save" : "Edit"}
+                      </Button>
+                    </div>
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label>{parentKeys[Location]}:</Form.Label>
+                    <div className="d-flex align-items-center">
+                      <Form.Control
+                        disabled={!editableFields[parentKeys[Location]]}
+                        type="text"
+                        className="ms-4 bg-light"
+                        style={{ width: "95%" }}
+                        defaultValue={data && data[parentKeys[Location]]}
+                        onChange={(e) =>
+                          handleInputChange(
+                            parentKeys[Location],
+                            e.target.value
+                          )
+                        }
+                      />
+                      <Button
+                        variant="secondary"
+                        className="ms-2"
+                        onClick={() => {
+                          if (editableFields[parentKeys[Location]]) {
+                            saveData(
+                              parentKeys[Location],
+                              editedValues[parentKeys[Location]]
+                            );
+                          }
+                          toggleEditMode(parentKeys[Location]);
+                        }}
+                      >
+                        {editableFields[parentKeys[Location]] ? "Save" : "Edit"}
+                      </Button>
+                    </div>
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label>{parentKeys[Description]}:</Form.Label>
+                    <div className="d-flex align-items-center">
+                      <textarea
+                        disabled={!editableFields[parentKeys[Description]]}
+                        className="form-control ms-4 bg-light"
+                        style={{ width: "95%" }}
+                        defaultValue={data && data[parentKeys[Description]]}
+                        onChange={(e) =>
+                          handleInputChange(
+                            parentKeys[Description],
+                            e.target.value
+                          )
+                        }
+                      ></textarea>
+                      <Button
+                        variant="secondary"
+                        className="ms-2"
+                        onClick={() => {
+                          if (editableFields[parentKeys[Description]]) {
+                            saveData(
+                              parentKeys[Description],
+                              editedValues[parentKeys[Description]]
+                            );
+                          }
+                          toggleEditMode(parentKeys[Description]);
+                        }}
+                      >
+                        {editableFields[parentKeys[Description]] ? "Save" : "Edit"}
+                      </Button>
+                    </div>
+                  </Form.Group>
+                </Form>
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
 }
 
-export default POIData;
+export default UserData;
--- src/Dashboard/UserData.js
+++ src/Dashboard/UserData.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import GetData from "./GatherData";
-import { Button, ListGroup } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
 import Card from "react-bootstrap/esm/Card";
@@ -16,11 +16,19 @@ function UserData() {
     [parentKeys[2]]: false,
     [parentKeys[1]]: false,
   });
-
+  const progressKeys = data ? Object.keys(data[parentKeys[3]]) : [];
+  const progressValues = data ? Object.values(data[parentKeys[3]]) : [];
   const [editedValues, setEditedValues] = useState({
     [parentKeys[2]]: "",
     [parentKeys[1]]: "",
   });
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (data !== null) {
+      setLoading(false);
+    }
+  }, [data]);
 
   const toggleEditMode = (field) => {
     setEditableFields({
@@ -35,12 +43,15 @@ function UserData() {
       if (field === parentKeys[2]) {
         //setParentKey(value); // Update parentKey state
       }
-
-      // Save data
-      const inputData = value;
+      var inputData = value;
       const dataToUpdate = {};
       dataToUpdate[field] = inputData;
-      set(ref(db, `Users/${ValueName}/${field}`), inputData);
+      if (progressKeys.includes(field)) {
+        inputData = parseInt(value);
+        set(ref(db, `Users/${ValueName}/Progression/${field}`), inputData);
+      } else {
+        set(ref(db, `Users/${ValueName}/${field}`), inputData);
+      }
     }
   };
 
@@ -50,6 +61,20 @@ function UserData() {
       [field]: value,
     });
   };
+
+  const handleSaveProgress = () => {
+    // Save all progress keys and values
+    Object.keys(editedValues[parentKeys[3]]).forEach((key) => {
+      saveData(key, editedValues[parentKeys[3]][key]);
+    });
+    // Toggle edit mode for all progress keys
+    toggleEditMode(parentKeys[3]);
+  };
+
+  if (loading) {
+    return <div>Fetching Data...</div>;
+  }
+  
   return (
     <>
       <Row>
@@ -78,7 +103,7 @@ function UserData() {
               </Col>
             </Row>
             <Row>
-              <Col md className="bg-white rounded-md-start-5 p-3">
+              <Col md className="bg-white rounded-start-5 p-3">
                 <Form>
                   <Form.Group>
                     <Form.Label>{parentKeys[4]}:</Form.Label>
@@ -161,8 +186,48 @@ function UserData() {
                   </Form.Group>
                 </Form>
               </Col>
-              <Col md className="bg-dark rounded-md-end-5 p-3">
-
+              <Col md className="bg-white rounded-end-5 p-3">
+                <Form>
+                  <Row>
+                    <Col md={10}>
+                      <Form.Group>
+                        <div className="d-flex">
+                          {progressKeys.map((key, index) => (
+                            <div key={index} className="me-3">
+                              <div>{key}:</div>
+                              <Form.Control
+                                disabled={!editableFields[parentKeys[3]]}
+                                type="number"
+                                className="bg-light"
+                                defaultValue={progressValues[index]}
+                                onChange={(e) =>
+                                  handleInputChange(parentKeys[3], {
+                                    ...editedValues[parentKeys[3]],
+                                    [key]: e.target.value,
+                                  })
+                                }
+                              />
+                            </div>
+                          ))}
+                        </div>
+                      </Form.Group>
+                    </Col>
+                    <Col md={2} className="p-3 align-self-end">
+                      <Button
+                        variant="secondary"
+                        className="mt-2"
+                        onClick={() => {
+                          if (editableFields[parentKeys[3]]) {
+                            handleSaveProgress();
+                          }
+                          toggleEditMode(parentKeys[3]);
+                        }}
+                      >
+                        {editableFields[parentKeys[3]] ? "Save" : "Edit"}
+                      </Button>
+                    </Col>
+                  </Row>
+                </Form>
               </Col>
             </Row>
           </Card>
