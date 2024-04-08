1f687247ffde1ca1cc8adc2306a27c41a8e7ed5d
TheIronVortex <sem.opstals@student.gildeopleidingen.nl>
2024-04-08 09:28:33 +0200

    Customization Functionality
    Page can be customized

--- src/Customize/Customize.js
+++ src/Customize/Customize.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Card, Col, Row } from 'react-bootstrap';
+import { Button, Card, Col, Row } from 'react-bootstrap';
 
 //const fs = require('fs');
 
 function Customize({ setSelectedColor, setSelectedTextColor }) {
   const [textColor, setTextColor] = useState(() =>{
     const textColor = localStorage.getItem('selectedTextColor');
-    return textColor ? textColor : '#000000';
+    return textColor ? textColor : '#ffffff';
   })
   const [colorPrimary, setColorPrimary] = useState(() => {
     const storedColor = localStorage.getItem('selectedColorPrimary');
@@ -25,26 +25,38 @@ function Customize({ setSelectedColor, setSelectedTextColor }) {
     setTextColor(newTextColor)
   };
 
+  const resetColor = () => {
+    setColorPrimary('#e20d18');
+    setTextColor('#ffffff');
+  };
+
   return (
     <Col>
       <Card className="p-5 card rounded-5" style={{ '--primary-color': colorPrimary, '--text-color': textColor }}>
         <h1 className='text-custom'>Customization</h1>
         <div className='p-4 rounded-5 bg-white'>
           <Row>
-            <Col className='col-auto'>
-              <h4>Primary Colour: </h4>
+            <Col>
+              <Row>
+                <Col className='col-auto'>
+                  <h4>Primary Colour: </h4>
+                </Col>
+                <Col>
+                  <input type="color" value={colorPrimary} onChange={(e) => handleColorChange(e.target.value, textColor)} />
+                </Col>
+              </Row>
+            <hr />
+              <Row>
+                <Col className='col-auto'>
+                  <h4>Text Colour: </h4>
+                </Col>
+                <Col>
+                  <input type="color" value={textColor} onChange={(e) => handleColorChange(colorPrimary, e.target.value)} />
+                </Col>
+              </Row>
             </Col>
             <Col>
-              <input type="color" value={colorPrimary} onChange={(e) => handleColorChange(e.target.value, textColor)} />
-            </Col>
-          </Row>
-        <hr />
-          <Row>
-            <Col className='col-auto'>
-              <h4>Text Colour: </h4>
-            </Col>
-            <Col>
-              <input type="color" value={textColor} onChange={(e) => handleColorChange(colorPrimary, e.target.value)} />
+              <Button className='btn'  onClick={resetColor}>Reset</Button>
             </Col>
           </Row>
         </div>
